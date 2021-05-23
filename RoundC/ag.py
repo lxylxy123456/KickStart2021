22295f73002875a86556b5b4b1e50a6a5be0eef1
李宵逸 <13691419520@163.com>
2021-05-23 14:40:33 -0700

    ag.py

--- RoundC/ag.py
+++ RoundC/ag.py
@@ -12,12 +12,49 @@ except Exception:
 
 # import math, sys
 # sys.setrecursionlimit(100000000)
-# from collections import defaultdict
+from collections import Counter
 # A = list(map(int, input().split()))
+
+def factor(a):
+	ans = Counter()
+	while a % 2 == 0:
+		ans[2] += 1
+		a //= 2
+	for i in range(3, a + 3):
+		if a == 1:
+			break
+		if i**2 > a:
+			ans[a] += 1
+			break
+		while a % i == 0:
+			ans[i] += 1
+			a //= i
+	return sorted(ans.items())
+
+def factors(factored, start=0):
+	if not factored[start:]:
+		yield 1
+	else:
+		a = 1
+		for i in range(factored[start][1] + 1):
+			for j in factors(factored, start + 1):
+				yield a * j
+			a *= factored[start][0]
 
 T = int(input())
 for test in range(T):
 	G = int(input())
-	ans = G
+	# Find x s.t. exists i >= 0, x + (x+1) ... + (x+i) = G
+	# (2*x + i) * (i + 1) = G * 2
+	ans = 0
+	for i_plus_1 in factors(factor(G * 2)):
+		i = i_plus_1 - 1
+		two_x = G * 2 // i_plus_1 - i
+		if two_x % 2 == 0 and two_x >= 1:
+			x = two_x // 2
+			# print(x)
+			ans += 1
 	print('Case #%d:' % (test + 1), ans)
+
+# 16 min, AC AC (1 attempts total)
 
